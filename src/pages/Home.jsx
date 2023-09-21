341bfaf7be3028c69e300cd3afc85ffdf000e0a9
MAC <mac@OMOBOLAJIs-MacBook-Pro.local>
2023-09-21 18:54:01 +0100

    resolved issues

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,76 @@
-import React from 'react'
 import Header from '../components/Header'
 import Gallery from '../components/Gallery'
+import { useEffect, useState } from 'react'
+import { auth } from '../firebase/firebase-config'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth'
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => { 
+
+  const [email,setEmail] = useState("")
+  const [password,setPassword] = useState("")
+  const [user, setUser] = useState(null)
+  const navigate = useNavigate()
+
+  useEffect(() => {
+      
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+        if(user){
+            setUser(user)
+        }else{
+            setUser(null)
+        }
+    })
+
+    return () => unsubscribe()
+},[])
+
+const handleLogin = async (e) => {
+    try{
+        e.preventDefault()
+        await createUserWithEmailAndPassword(auth,"user@example.com","@1Password")
+        await signInWithEmailAndPassword(auth,email,password)
+        navigate("/")
+    }catch(e){
+        console.log(e.message);
+    }
+}
   return (
     <div className='mx-8'>
         <Header/>
-        <Gallery/>
+        {user ? (
+          <Gallery/>
+        ):(
+          <div className='flex h-screen items-center justify-center'>
+            {user ? (
+                <p>Login Successful</p>
+            ): (
+                <form className='flex flex-col gap-8 items-start border-2 p-16 max-w-[400px] w-[90%] border-black rounded-md' onSubmit={(e) => handleLogin(e)}>
+                    <div className='flex flex-col gap-4 w-full'>
+                        <label htmlFor="email">Email</label>
+                        <input 
+                            type="email"
+                            id='email'
+                            placeholder='Enter Email' 
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            className='border-2 px-4 py-2 placeholder:text-black bg-[#f2f2f2] border-black rounded-md outline-none'/>
+                    </div>
+                    <div className='flex flex-col gap-4 w-full'>
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password" 
+                            id='password'
+                            placeholder='Enter Password' 
+                            value={password}
+                            onChange={(e) => setPassword(e.target.value)}
+                            className='border-2 px-4 py-2 placeholder:text-black bg-[#f2f2f2] border-black rounded-md outline-none'/>
+                    </div>
+                    <button className='w-full bg-[#212121] text-white px-4 py-2 rounded-md'>Login</button>
+                </form>
+            )}
+        </div>
+        )}
     </div>
   )
 }
