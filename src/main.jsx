d9e0dc3c1456645e80feb4fe316e2d5c6c190187
MAC <mac@OMOBOLAJIs-MacBook-Pro.local>
2023-09-21 22:08:07 +0100

    resolved issues

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,11 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/Home.jsx'
-import Login from './pages/Login.jsx'
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Home/>
-  },
-  {
-    path: "/login",
-    element: <Login/>
   },
 ])
 
